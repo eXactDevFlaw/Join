b291761233bedc26c55dc380bd744924c51fef5f
Lutz Bölling <lutzboelling@gmx.de>
2025-05-29 17:54:04 +0200

    added animation on landing + styleing

--- scripts/login.js
+++ scripts/login.js
@@ -1,2 +1,14 @@
 let userName = document.getElementById('username');
 let userPassword = document.getElementById('userpassword')
+
+window.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    
+    let hiddenItems = document.querySelectorAll('.fade_out');
+   
+    hiddenItems.forEach(function(element) {
+      element.classList.add('fade_in');
+      element.classList.remove('fade_out');
+    });
+  }, 1000);
+});
