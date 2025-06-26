306938ef0e21d3d474c8d3c70861a2ea7b232f01
CK <christianklemm1987@gmail.com>
2025-06-26 11:52:08 +0200

    add task overlay assigned to dropdown arrow changed

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -115,3 +115,24 @@ function createTask() {
     console.log(taskDetails);
 
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function openAssignedToDropdown() {
+//   document.getElementById("myDropdown").classList.toggle("d_none");
+  document.getElementById("arrow-drop-down").classList.toggle("up")
+}
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
